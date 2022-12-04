372e15ac1d7b47e99db834b7dfbc2c33d337bcff
Arun Pandiyan <41441589+LonelyKing26@users.noreply.github.com>
2022-12-04 18:44:50 +0530

    Add files via upload

---
+++ assets/js/get_imdb_data.js
@@ -0,0 +1,13 @@
+
+      $("#get_data").click(function(){
+        var imdb = $('#imdbid').val();
+       
+        if(imdb = ''){
+          alert('Please Fill IMDb ID');
+        }
+        else{
+            
+            }
+
+      });
+
